4149572aff156bd0002d10c496c5f34c30fd6f3a
LUCAS EIAL BENEZRA <48313633@ort>
2025-04-03 10:40:24 -0300

    Casi Terminado

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const inputTODO=document.getElementById("TODOIngresado");
 const botonAgregar=document.getElementById("AgregarTODO");
+const container = document.getElementById('lista-container');
 let ListaTODO=[];
 botonAgregar.onclick=()=>{
-    event.preventDefault();
     if(inputTODO!=''){
         ListaTODO.push({
             razon:inputTODO.value,
@@ -14,14 +14,26 @@ botonAgregar.onclick=()=>{
         agregarToDo();
     }
 }
-function agregarToDo(){
-    let container=document.getElementById('lista-container');
+function agregarToDo() {
     container.innerHTML='';
-    ListaTODO.forEach(elementoToDo => {
+    ListaTODO.forEach((elementoToDo,index)=>{
         let nuevoToDo=document.createElement('label');
-        nuevoToDo.innerHTML=`
-        <input type=checkbox /> ${elementoToDo.razon}
-        `
+        nuevoToDo.innerHTML=`<input type="checkbox" class="chbox" data-index="${index}"/>${elementoToDo.razon}`;
         container.appendChild(nuevoToDo);
     });
 }
+document.getElementById('lista-container').onclick = (e) => {
+    if (e.target.classList.contains('chbox')) {
+        let index = e.target.dataset.index;
+        let labelPadre = e.target.parentNode;
+        if (e.target.checked) {
+            ListaTODO[index].hecho = true;
+            ListaTODO[index].fechaTerminado = new Date(Date.now()).toDateString();
+            labelPadre.classList.add('terminado');
+        } else {
+            ListaTODO[index].hecho = false;
+            ListaTODO[index].fechaTerminado = undefined;
+            labelPadre.classList.remove('terminado');
+        }
+    }
+};
